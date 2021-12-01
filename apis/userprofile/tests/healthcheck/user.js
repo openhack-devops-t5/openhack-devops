95344034cd3c4b3c170bcea902def2ce9e7c5a7a
Tomas Slavik <tomas.slavik@microsoft.com>
2021-12-01 13:39:38 +0100

    test fail

--- apis/userprofile/tests/healthcheck/user.js
+++ apis/userprofile/tests/healthcheck/user.js
@@ -33,7 +33,7 @@ Test('/healthcheck/user', function (t) {
          */
         t.test('test  get operation', function (t) {
             Mockgen().requests({
-                path: '/healthcheck/user',
+                path: '/healthcheck/user2',
                 operation: 'get'
             }, function (err, mock) {
                 var request;
